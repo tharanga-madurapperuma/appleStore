aec5ff32cc87997e1d6f89b4dba8f7c3ad33d7eb
tharanga_madurapperuma <tharanga.s.madurapperuma@gmail.com>
2024-08-08 21:51:35 +0530

    Making the form validation

--- src/account/account.js
+++ src/account/account.js
@@ -1,4 +1,41 @@
 const validation = () => {
-    console.log("Submit button clicked");
-    return false; 
+    if(checkFirstName() && checkLastName() && checkEmail() && checkPassword() && checkPhoneNumber()){
+        return true;
+    }
+    else{
+        false;
+    }
+}
+
+function checkFirstName(){
+    const fName = document.getElementById("firstName").value;
+
+    if(!/^[a-zA-Z]+$/.test(fName)){
+        document.getElementById("firstNameError").innerHTML = "Only Alphebets are allowed";
+        return false;
+    }
+    else{
+        return true;
+    }
+    
+};
+
+function checkLastName(){
+
+    return true;
+}
+
+function checkEmail(){
+
+    return true;
+}
+
+function checkPassword(){
+
+    return true;
+}
+
+function checkPhoneNumber(){
+
+    return true;
 }
