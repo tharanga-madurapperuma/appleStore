3b4bc4c02b5512dd5d95ed9e24014ae3a36e7054
tharanga_madurapperuma <tharanga.s.madurapperuma@gmail.com>
2024-08-09 23:15:33 +0530

    Working on javascript validation

--- src/account/account.js
+++ src/account/account.js
@@ -3,30 +3,40 @@ const validation = () => {
         return true;
     }
     else{
-        false;
+        console.log("returned False");
+        return false;
     }
 }
 
 function checkFirstName(){
     const fName = document.getElementById("firstName").value;
 
-    if(!/^[a-zA-Z]+$/.test(fName)){
+    if(!(/^[A-Za-z]+$/.test(fName))){
         document.getElementById("firstNameError").innerHTML = "Only Alphebets are allowed";
         return false;
     }
     else{
+        document.getElementById("firstNameError").innerHTML = "";
         return true;
     }
     
 };
 
 function checkLastName(){
+    const lName = document.getElementById("lastName").value;
 
-    return true;
+    if(!(/^[A-Za-z]+$/.test(lName))){
+        document.getElementById("lastNameError").innerHTML = "Only Alphebets are allowed";
+        return false;
+    }
+    else{
+        document.getElementById("lastNameError").innerHTML = "";
+        return true;
+    }
 }
 
 function checkEmail(){
-
+    
     return true;
 }
 
